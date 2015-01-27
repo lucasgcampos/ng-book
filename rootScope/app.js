ed4ea2f745f7035484560b964d9a334896b4bcf9
Lucas Campos <lucasgcampos.contato@gmail.com>
2015-01-27 16:02:15 -0200

    Refatorando exemplo simples de

--- rootScope/app.js
+++ rootScope/app.js
@@ -1,3 +1,3 @@
-angular.module('myApp', []).run(function($rootScope) {
-	$rootScope.name = "World";
+angular.module('myApp', []).controller('MyController', function($scope) {
+	$scope.name = "Lucas Campos";
 });
