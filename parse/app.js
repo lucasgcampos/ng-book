56d2fe3744c835d6edd4c9cfd8816348e2de4544
Lucas Campos <lucasgcampos.contato@gmail.com>
2015-01-28 10:09:28 -0200

    AngularJS com

---
+++ parse/app.js
@@ -0,0 +1,8 @@
+angular.module('myApp', []).controller('MyController', function($scope, $parse) {
+	$scope.$watch('expr', function(newVal, oldVal, scope) {
+		if (newVal !== oldVal) {
+			var parseFun = $parse(newVal);
+			$scope.parsedValue = parseFun(scope);	
+		}
+	});
+}); 
