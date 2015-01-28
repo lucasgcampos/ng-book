6d0dcfea2f05fb4682286a010d5b6186e13ba0d3
Lucas Campos <lucasgcampos.contato@gmail.com>
2015-01-28 11:56:52 -0200

    Filtros

--- filtros/app.js
+++ filtros/app.js
@@ -4,7 +4,8 @@ angular.module('myApp', []).controller('MyController', function($scope) {
 		lowercase: "LOWERCASE",
 		limitDecimal: 12.3456,
 		currency: 123.698,
-		date: new Date()
-
+		date: new Date(),
+		string: "string",
+		array: [1, 2, 3, 4, 5, 6, 7 , 8, 9, 0]
 	};
 });
